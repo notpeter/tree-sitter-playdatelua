e86218fbf5260ea8b001f7eb9d562bff87015f0d
Peter Tripp <petertripp@gmail.com>
2026-01-16 11:00:07 -0500

    Support Compound Assignment Operators (Playdate Lua) (#1)
    Add support for Compound Assignment Operators as used in the Panic Playdate SDK version of Lua.

--- grammar.js
+++ grammar.js
@@ -117,7 +117,21 @@ export default grammar({
     assignment_statement: ($) =>
       seq(
         alias($._variable_assignment_varlist, $.variable_list),
-        field('operator', '='),
+        choice(
+          field('operator', '='),
+          // Additional non-standard assignment operators used by Playdate Lua
+          field("operator", "+="),
+          field("operator", "-="),
+          field("operator", "*="),
+          field("operator", "//="),
+          field("operator", "/="),
+          field("operator", "%="),
+          field("operator", "<<="),
+          field("operator", ">>="),
+          field("operator", "&="),
+          field("operator", "|="),
+          field("operator", "^="),
+        ),
         alias($._variable_assignment_explist, $.expression_list)
       ),
     // varlist ::= var {',' var}
@@ -496,7 +510,7 @@ export default grammar({
           '[',
           field('name', $.expression),
           ']',
-          field('operator', '='),
+          choice(field("operator", "=")),
           field('value', $.expression)
         ),
         seq(field('name', $.identifier), field('operator', '='), field('value', $.expression)),
